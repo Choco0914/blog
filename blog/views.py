7e2f6cead58229465821bc5ad34e6132a9550149
Choco0914 <heojeongho1992@gmail.com>
2018-09-05 15:58:28 +0900

    basic_view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
+"""
+post_list라는 함수(def)를 만들어 요청(request) 을 인자로 넘겨받아 render 메서드를
+호출한다. 이 함수는 호출하여 받은(return) blog/post_list 템플릿을 보여준다
+"""
