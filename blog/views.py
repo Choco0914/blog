169125b511d965d2e0ef82061da34dd40d4204cd
Choco0914 <heojeongho1992@gmail.com>
2018-09-07 18:51:28 +0900

    Expend app!

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
 
@@ -9,3 +9,6 @@ def post_list(request):
 post_list라는 함수(def)를 만들어 요청(request) 을 인자로 넘겨받아 render 메서드를
 호출한다. 이 함수는 호출하여 받은(return) blog/post_list 템플릿을 보여준다
 """
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
